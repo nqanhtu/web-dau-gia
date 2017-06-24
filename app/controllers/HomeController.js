ef4174fb312e8329d80ab34c26070894b97d96f8
Tu Le Minh <tuleminh396@gmail.com>
2017-06-24 16:51:58 +0700

    k

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,7 +1,31 @@
 var productModel = require('../models/productModel');
 
 var homeController = {
+    index: function (req, res) {
+        productModel.loadAllProduct()
+            .then(function (products) {
+                productModel.loadLastestBidder().then(function (rows) {
 
+                    for(var i = 0; i < products.length; ++i) {
+                        products[i]['Bids'] = 0;
+                    };
+
+                    for (var i = 0; i < rows.length; ++i) {
+                        for (var j = 0; j < products.length; ++j) {
+                            if (products[j].ID == rows[i].ProductID) {
+                                products[j].Bids = rows[i].Bids;
+                            };
+                        };
+                    };
+
+                    res.render('home', {
+                        layout: 'main',
+                        products: products,
+                        bids: rows
+                    });
+                });
+            });
+    },
 };
 
 module.exports = homeController;
