402921037abcb0db8118eed3759ce7af6c51eae6
Tu Le Minh <tuleminh396@gmail.com>
2017-06-25 10:31:01 +0700

    update

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -6,18 +6,22 @@ module.exports = {
     getUserByEmail: function (email) {
         var deffered = Q.defer();
         var sql = 'select * from users where email = ' + '\'' + email + '\'';
+
         db.load(sql).then(function (rows) {
             deffered.resolve(rows[0]);
         });
+        
         return deffered.promise;
     },
 
     getUserByID: function (id, callback) {
         var deffered = Q.defer();
         var sql = 'select * from users where id = ' + id;
+
         db.load(sql).then(function (rows) {
             deffered.resolve(rows[0]);
         });
+
         return deffered.promise;
     },
 
@@ -27,5 +31,12 @@ module.exports = {
      */
     GetAllUsers: function () {
         var deffered = Q.defer();
+        var sql = 'SELECT id, full_name FROM users WHERE admin = 0;'
+
+        db.load(sql).then(function (rows) {
+            deffered.resolve(rows);
+        });
+
+        return deffered.promise;
     }
 };
