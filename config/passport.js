3b24516c79f6b906e6bf5dc832542541f59ca26e
Anh Tu <ngqanhtu@outlook.com>
2017-06-29 22:38:02 +0700

    recapcha

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,8 @@ var bcrypt = require('bcrypt-nodejs');
 var crypto = require('crypto');
 var database = require('../middlewares/db_helpers');
 var mustache = require('mustache');
+var bodyParser = require('body-parser');
+var request = require('request');
 
 module.exports = function (passport) {
 
@@ -29,32 +31,52 @@ module.exports = function (passport) {
             passwordField: 'password',
             passReqToCallback: true
         },
-        function (req, email, password, done) {
-            var query = 'SELECT * FROM users WHERE email = "' + email + '"';
-            database.passportSelect(query, function (error, rows) {
-                if (error)
-                    return done(error);
-                if (rows.length) {
-                    return done(null, false, req.flash('registerMessage', 'That username is already taken.'));
-                } else {
-                    var newUser = {
-                        email: email,
-                        full_name: req.body.fullName,
-                        address: req.body.address,
-                        avatar: req.body.avatar,
-                        password: crypto.createHash('md5').update(password).digest('hex')
-                    };
-                    var insertNewUserQuery = mustache.render(
-                        'INSERT INTO `users` (`full_name`, `email`, `avatar`, `address`, `password`) VALUES ("{{full_name}}", "{{email}}", "{{address}}", "{{avatar}}", "{{password}}")',
-                        newUser);
+            function (req, email, password, done) {
 
-                    database.passportInsert(insertNewUserQuery, function (error, result) {
-                        newUser['id'] = result.insertId;
-                        return done(null, newUser);
-                    });
+                //reCapCha
+                if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+                    return res.json({ "responseCode": 1, "responseDesc": "Please select captcha" });
                 }
-            });
-        })
+                // Put your secret key here.
+                var secretKey = "6LcWVScUAAAAAIaqzgtahZvqUClIxb-CD9GHNcim";
+                // req.connection.remoteAddress will provide IP address of connected user.
+                var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+                // Hitting GET request to the URL, Google will respond with success or error scenario.
+                request(verificationUrl, function (error, response, body) {
+                    body = JSON.parse(body);
+                    // Success will be true or false depending upon captcha validation.
+                    if (body.success !== undefined && !body.success) {
+                        return res.json({ "responseCode": 1, "responseDesc": "Failed captcha verification" });
+                    }
+                    res.json({ "responseCode": 0, "responseDesc": "Sucess" });
+                });
+
+
+                var query = 'SELECT * FROM users WHERE email = "' + email + '"';
+                database.passportSelect(query, function (error, rows) {
+                    if (error)
+                        return done(error);
+                    if (rows.length) {
+                        return done(null, false, req.flash('registerMessage', 'That username is already taken.'));
+                    } else {
+                        var newUser = {
+                            email: email,
+                            full_name: req.body.fullName,
+                            address: req.body.address,
+                            avatar: req.body.avatar,
+                            password: crypto.createHash('md5').update(password).digest('hex')
+                        };
+                        var insertNewUserQuery = mustache.render(
+                            'INSERT INTO `users` (`full_name`, `email`, `avatar`, `address`, `password`) VALUES ("{{full_name}}", "{{email}}", "{{address}}", "{{avatar}}", "{{password}}")',
+                            newUser);
+
+                        database.passportInsert(insertNewUserQuery, function (error, result) {
+                            newUser['id'] = result.insertId;
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            })
     );
 
     passport.use(
@@ -78,7 +100,7 @@ module.exports = function (passport) {
                     if (!(encryptedPassword === rows[0].password))
                         return done(null, false, req.flash('loginMessage', 'That password is incorrect. Try again.'));
 
-                    return done(null, rows[0]);                    
+                    return done(null, rows[0]);
                 });
             })
     );
