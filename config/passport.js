fde48ca5a444b37276a7bd6d81795623540e9a21
Anh Tu <ngqanhtu@outlook.com>
2017-06-30 11:24:56 +0700

    thêm recaptcha vô passportjs

--- config/passport.js
+++ config/passport.js
@@ -32,34 +32,64 @@ module.exports = function (passport) {
         },
             function (req, res, email, password, done) {
 
+                let request = require('request');    // Sử dụng thư viện request để gửi request lên Google API
+                let data = req.body;  // Dữ liệu từ form submit lên bao gồm thông tin đăng ký và captcha response
+                let captchaResponse = data.captchaResponse;
+
+                if (captchaResponse) {
+                    request({
+                        url: 'https://www.google.com/recaptcha/api/siteverify',
+                        method: 'POST',
+                        form: {
+                            secret: '6LcWVScUAAAAAIaqzgtahZvqUClIxb-CD9GHNcim',
+                            response: captchaResponse
+                        }
+                    }, function (error, response, body) {
+                        // Parse String thành JSON object
+                        try {
+                            body = JSON.parse(body);
+                        } catch (err) {
+                            body = {};
+                        }
+
+                        if (!error && response.statusCode == 200 && body.success) {
+                            // Captcha hợp lệ, xử lý tiếp phần đăng ký tài khoản    
+                            var query = 'SELECT * FROM users WHERE email = "' + email + '"';
+                            database.passportSelect(query, function (error, rows) {
+                                if (error)
+                                    return done(error);
+                                if (rows.length) {
+                                    return done(null, false, req.flash('registerMessage', 'That username is already taken.'));
+                                } else {
+                                    var newUser = {
+                                        email: email,
+                                        full_name: req.body.fullName,
+                                        address: req.body.address,
+                                        avatar: req.body.avatar,
+                                        password: crypto.createHash('md5').update(password).digest('hex')
+                                    };
+                                    var insertNewUserQuery = mustache.render(
+                                        'INSERT INTO `users` (`full_name`, `email`, `avatar`, `address`, `password`) VALUES ("{{full_name}}", "{{email}}", "{{address}}", "{{avatar}}", "{{password}}")',
+                                        newUser);
+
+                                    database.passportInsert(insertNewUserQuery, function (error, result) {
+                                        newUser['id'] = result.insertId;
+                                        return done(null, newUser);
+                                    });
+                                }
+                            });
+
+                        } else {
+                            // Xử lý lỗi nếu Captcha không hợp lệ
+                        }
+                    });
+                } else {
+                    // Xử lý lỗi nếu không có Captcha
+                }
 
 
 
 
-                var query = 'SELECT * FROM users WHERE email = "' + email + '"';
-                database.passportSelect(query, function (error, rows) {
-                    if (error)
-                        return done(error);
-                    if (rows.length) {
-                        return done(null, false, req.flash('registerMessage', 'That username is already taken.'));
-                    } else {
-                        var newUser = {
-                            email: email,
-                            full_name: req.body.fullName,
-                            address: req.body.address,
-                            avatar: req.body.avatar,
-                            password: crypto.createHash('md5').update(password).digest('hex')
-                        };
-                        var insertNewUserQuery = mustache.render(
-                            'INSERT INTO `users` (`full_name`, `email`, `avatar`, `address`, `password`) VALUES ("{{full_name}}", "{{email}}", "{{address}}", "{{avatar}}", "{{password}}")',
-                            newUser);
-
-                        database.passportInsert(insertNewUserQuery, function (error, result) {
-                            newUser['id'] = result.insertId;
-                            return done(null, newUser);
-                        });
-                    }
-                });
             })
     );
 
